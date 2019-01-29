7b0b219b0a99ad38429d0dbf7fcb6078a2e42880
Harrison McCarty <harrisonmccarty301@gmail.com>
2019-01-28 23:40:57 -0500

    Updated ProjectX architecture in preparation for practice bot

---
+++ ProjectX/src/main/java/frc/subsystem/superstructure.java
@@ -0,0 +1,9 @@
+package frc.subsystem; 
+
+public class superstructure {
+    public void addLoop(){}
+    public void startUp(){}
+    public void run(){}
+    public void end(){}
+    //public void enactState(stateType subsystem, ){}
+}
