24323e4f1e3f4760aec403ab10457e8213d8f5bf
Jacob A <Robotics@HSD104-SL01.brownsburg.k12.in.us>
2019-01-29 21:06:01 -0500

    Ultrasonic Sensor Code Started

--- JacobDev/src/main/java/frc/robot/Robot.java
+++ JacobDev/src/main/java/frc/robot/Robot.java
@@ -4,10 +4,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Ultrasonic;
 
 public class Robot extends IterativeRobot {
   private static final String kDefaultAuto = "Default";
@@ -15,22 +12,14 @@ public class Robot extends IterativeRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  Joystick Stick = new Joystick(0);
-  Compressor Compressor = new Compressor();
-  Solenoid blue1 = new Solenoid(2);
-  Solenoid blue2 = new Solenoid(3);
-  Solenoid purple1 = new Solenoid(0);
-  Solenoid purple2 = new Solenoid(1);  
-  Timer timer =  new Timer();
-  int cycler = 1;
+  private double range;
+  Ultrasonic ballDetector = new Ultrasonic(1, 1); //Ultrasonic(pingChannel, echoChannel)
 
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    timer.start();
-    timer.reset();
   }
 
   
@@ -64,92 +53,11 @@ public class Robot extends IterativeRobot {
     }
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
   @Override
   public void teleopPeriodic()
   {   
-    if(Stick.getRawButtonPressed(5)){
-      timer.reset();
-      cycler = 1;
-    }
-    else if (Stick.getRawButtonPressed(3)){
-      cycler = 2;
-    }
-    else if (Stick.getRawButtonPressed(4)){
-      cycler = 3; 
-    }
-    switch(cycler){
-      case 1:
-        outtake();
-        break; 
-      case 2:
-        hold();
-        break;
-      case 3:
-        intake();
-        break;
-    }
-    
-    if(Stick.getRawButton(2))
-    {
-      Compressor.stop();
-    }
-    else
-    {
-      Compressor.start();
-    }
-  }
-
-  public void intake()
-  {
-    setBlue(true);
-    setPurple(false);
-  }
-
-  public void hold()
-  {
-    setPurple(true);
-  }
-
-  public void outtake()
-  {
-    
-    setBlue(false);
-    if(timer.get() > 0.5)
-    {
-      setPurple(false);
-      timer.reset();
-    }
-  }
-
-  public void setBlue(boolean on)
-  {
-    if(on)
-    {
-      blue1.set(true);
-      blue2.set(false);
-    }
-    else
-    {
-      blue1.set(false);
-      blue2.set(true);
-    }
-  }
-
-  public void setPurple(boolean on)
-  {
-    if(on)
-    {
-      purple1.set(true);
-      purple2.set(false);
-    }
-    else
-    {
-      purple1.set(false);
-      purple2.set(true);
-    }
+    range = ballDetector.getRangeInches();
+    SmartDashboard.putNumber("Distance", range);
   }
 
   @Override
