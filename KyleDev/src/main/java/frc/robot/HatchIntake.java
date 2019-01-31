602c4d78a18c266364dfe04222dae08bccde9c91
kphillips2003 <45471789+kphillips2003@users.noreply.github.com>
2019-01-31 18:30:55 -0500

    1/29 Hatch Update

--- KyleDev/src/main/java/frc/robot/HatchIntake.java
+++ KyleDev/src/main/java/frc/robot/HatchIntake.java
@@ -7,8 +7,60 @@
 
 package frc.robot;
 
+import javax.lang.model.util.ElementScanner6;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.Timer;
+
 /**
  * Add your docs here.
  */
 public class HatchIntake {
+    static HatchIntake instance = new HatchIntake();
+    Joystick Joy;
+    Talon Tal1 = new Talon(0);
+    Talon Tal2 = new Talon(1);
+    Timer Tim1 = new Timer();
+
+  public HatchIntake(){
+    Joy = new Joystick(0);
+  }
+
+  public static HatchIntake getInstance() {
+    return instance; 
+  }
+  public void HatchIng() {      
+    while (Joy.getRawButtonPressed(10) == true) {
+      Tal1.set(1);
+      if (Joy.getRawButton(10) == false/*sensor*/) {
+        Tal1.set(0);
+        break;
+      }
+    }
+    if (Joy.getRawButtonPressed(11) == true) {
+      Tim1.start();
+      if (Tim1.get() < 7) {
+        Tal2.set(-1);
+      }
+      else {
+        Tal2.set(0);
+        Tim1.stop();
+        Tim1.reset();
+      }
+    }
+    if (Joy.getRawButtonPressed(12) == true) {
+      Tim1.start();
+      if (Tim1.get() < 7) {
+        Tal2.set(1);
+      }
+      else {
+        Tal2.set(0);
+        Tim1.stop();
+        Tim1.reset();
+      }
+    }
+  }
 }
