ef2ca2d92b9d3f1c35a76f6f6f4ecfdf68646d9b
SmartNerdGuy <43546211+SmartNerdGuy@users.noreply.github.com>
2019-01-30 19:24:29 -0500

    Update CargoIntake.java

--- SamDev/src/main/java/frc/robot/CargoIntake.java
+++ SamDev/src/main/java/frc/robot/CargoIntake.java
@@ -14,26 +14,23 @@ import edu.wpi.first.wpilibj.Joystick;
  * Anna is also doing this.
  */
 public class CargoIntake {
+    
+    private static CargoIntake instance = new CargoIntake();
     Talon inMotor;
     Talon upMotor;
     Joystick stick;
     
-    public void CargoIntake getInstance(){
+    public static CargoIntake getInstance() {
         return instance;
     }
     
-    inMotor = new Talon(0);
-    upMotor = new Talon(1);
-    stick = new Joystick(0);
+    private CargoIntake() {
+        inMotor = new Talon(0);
+        upMotor = new Talon(1);
+        stick = new Joystick(0);
+    }
 
     public void checkButtons() {
-        switch (Boolean.toString(stick.getRawButtonPressed(10))) {
-            case "true":
-                System.out.println("Button 10 is pressed.");
-                break;
-            case "false":
-                System.out.println("Button 10 is not pressed.");
-        }
         switch (Boolean.toString(stick.getRawButtonPressed(11))) {
             case "true":
                 System.out.println("Button 11 is pressed.");
@@ -41,16 +38,17 @@ public class CargoIntake {
             case "false":
                 System.out.println("Button 11 is not pressed.");
         }
+        switch (Boolean.toString(stick.getRawButtonPressed(12))) {
+            case "true":
+                System.out.println("Button 12 is pressed.");
+                break;
+            case "false":
+                System.out.println("Button 12 is not pressed.");
+        }
         System.out.println("All button values are recognized.");
     }
     
     public void cargoIntake() {
-        while (stick.getRawButtonPressed(11) == true) /*camera recognizes cargo near enough to intake*/ {
-            upMotor.set(-1);
-            if (true) /*sensor input*/ {
-                upMotor.set(0);
-                break;
-            }
-        }
+        
     }
 }
