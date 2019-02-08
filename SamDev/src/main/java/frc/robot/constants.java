33e76fc7ac10ffb81601f67d39a0649a34846a55
Jonathan Heidegger <jth3141@gmail.com>
2019-02-07 21:08:53 -0500

    2/7 Update

--- SamDev/src/main/java/frc/robot/constants.java
+++ SamDev/src/main/java/frc/robot/constants.java
@@ -10,4 +10,7 @@ public class constants {
     public static int HATCH_INTAKE_ACTUATOR = 6; 
     public static int HATCH_INTAKE_ROLLER = 7;
     public static int HATCH_IRSENSOR = 8;
+    public static final double CARGO_KP = 1;
+    public static final double CARGO_KI = 1;
+    public static final double CARGO_KD = 1;
 }
