89426b2aa1780307281edc8894c90dc17672b105
elijah-furuness <46844817+elijah-furuness@users.noreply.github.com>
2019-02-09 15:59:01 -0500

    2/9 Update

--- HarrisonREVDEV/src/main/java/frc/robot/Main.java
+++ HarrisonREVDEV/src/main/java/frc/robot/Main.java
@@ -14,10 +14,9 @@ import edu.wpi.first.wpilibj.RobotBase;
  * Unless you know what you are doing, do not modify this file except to
  * change the parameter class to the startRobot call.
  */
-import edu.wpi.first.wpilibj.RobotBase;
-
 public final class Main {
-  private Main() {}
+  private Main() {
+  }
 
   /**
    * Main initialization function. Do not perform any initialization here.
