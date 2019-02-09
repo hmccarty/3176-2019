b3987dae29e83662cdac4d0c895ac0da49667cf8
kphillips2003 <45471789+kphillips2003@users.noreply.github.com>
2019-02-08 20:56:38 -0500

    2/8 Hatch Intake Update

--- KyleDev/src/main/java/frc/robot/HatchIntake.java
+++ KyleDev/src/main/java/frc/robot/HatchIntake.java
@@ -7,109 +7,87 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.constants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class HatchIntake {
     private static HatchIntake instance = new HatchIntake();
-    private static Joystick Joy;
-    private static Timer Tim1;
     private static DigitalInput isDown;
     private static DigitalInput isUp;
     private static DigitalInput Sen;
-    private static Talon actuator;
     private static Talon roller;
     private static Solenoid Sol1;
     private static Solenoid Sol2;
+    private static DoubleSolenoid SolDo;
 
-    private  HatchIntake(){
-    Joy = new Joystick(0);
-    Tim1 = new Timer();
-    isDown = new DigitalInput(constants.HATCH_INTAKE_DOWN);
-    isUp = new DigitalInput(constants.HATCH_INTAKE_UP);
-    Sen = new DigitalInput(constants.HATCH_IR_SENSOR);
-    actuator = new Talon(constants.HATCH_INTAKE_ACTUATOR);
-    roller = new Talon(constants.HATCH_INTAKE_ROLLER); 
-    Sol1 = new Solenoid(0);
-    Sol2 = new Solenoid(1);
+    private HatchIntake(){
+      isDown = new DigitalInput(constants.HATCH_INTAKE_DOWN);
+      isUp = new DigitalInput(constants.HATCH_INTAKE_UP);
+      Sen = new DigitalInput(constants.HATCH_IR_SENSOR);
+      roller = new Talon(constants.HATCH_INTAKE_ROLLER); 
+      Sol1 = new Solenoid(0);
+      Sol2 = new Solenoid(1); 
+      SolDo = new DoubleSolenoid(1, 2);
   }
 
-  public static HatchIntake getInstance() {
-    return instance; 
-  }
+    public static HatchIntake getInstance() {
+      return instance; 
+    }
+    public void DoSol() {
+      SolDo.set(DoubleSolenoid.Value.kOff);
+      SolDo.set(DoubleSolenoid.Value.kForward);
+      SolDo.set(DoubleSolenoid.Value.kReverse);
+    }
+    public boolean getDown() {
+      return isDown.get();
+    }
 
-  public boolean getDown() {
-    return isDown.get();
-  }
+    public boolean getUp() {
+      return isUp.get();
+    }
 
-  public boolean getUp() {
-    return isUp.get();
-  }
+    public void IntakeUp() {
+      if (getDown()) {
+        DoSol();
+        getDown();
+      }
+      else {
+        getDown();
+      }
+    }
+    public void IntakeDown() {
+      if (getUp()) {
+        DoSol();
+        getUp();
+      }
+      else {
+        getUp();
+      }
+    }   
 
-  public void IntakeUp() {
-    if (getDown()) {
-      Sol1.set(true);
-      Sol2.set(false);
-      getDown();
-    }
-    else {
-      getDown();
-    }
-  }
-  public void IntakeDown() {
-    if (getUp()) {
-      Sol1.set(false);
-      Sol2.set(true);
-      getUp();
-    }
-    else {
-      getUp();
-    }
-  }   
-
-/*
-  public boolean IntakeUp() {
-    if(isDown.get()) {
-        actuator.set(0);
-        return true; 
-    }
-    else {
-        actuator.set(-.3);
-        return false; 
-    }
-  }
-
-  public boolean IntakeDown() {
-    if(isUp.get()) {
-        actuator.set(0);
+    public boolean IntoIntake(double speed) {
+      if (Sen.get()) {
+        roller.set(0);
         return true;
-    }
-    else {
-        actuator.set(.3);
+      }
+      else {
+        roller.set(speed);
         return false;
+      } 
     }
-  } */
-  public boolean IntoIntake(double speed) {
-    if (Sen.get()) {
-      roller.set(0);
-      return true;
-    }
-    else {
-      roller.set(speed);
-      return false;
-    } 
-  }
-  public void ZeroSensors() {
+    public void ZeroSensors() {
     
-  }
-  public void OutputToDash() {
-    SmartDashboard.putBoolean("Is Down:", isDown.get());
-    SmartDashboard.putBoolean("Is Up:", isUp.get());
-    SmartDashboard.putBoolean("Sensor:", Sen.get());
+    }
+    public void OutputToDash() {
+      SmartDashboard.putBoolean("Is Down:", isDown.get());
+      SmartDashboard.putBoolean("Is Up:", isUp.get());
+      SmartDashboard.putBoolean("Sensor:", Sen.get());
+      SmartDashboard.putBoolean("Solenoid 1:", Sol1.get());
+      SmartDashboard.putBoolean("Solenoid 2:", Sol2.get());
+
   }
 }
--- KyleDev/src/main/java/frc/robot/constants.java
+++ KyleDev/src/main/java/frc/robot/constants.java
@@ -1,13 +1,9 @@
 package frc.robot; 
 
 public class constants {
-    public static int CARGO_INTAKE_DOWN = 0; 
-    public static int CARGO_INTAKE_UP = 1;
-    public static int CARGO_INTAKE_ACTUATOR = 2; 
-    public static int CARGO_INTAKE_ROLLER = 3; 
+
     public static int HATCH_INTAKE_DOWN = 0; 
     public static int HATCH_INTAKE_UP = 1;
-    public static int HATCH_INTAKE_ACTUATOR = 2; 
-    public static int HATCH_INTAKE_ROLLER = 3; 
-    public static int HATCH_IR_SENSOR = 4;
+    public static int HATCH_INTAKE_ROLLER = 2; 
+    public static int HATCH_IR_SENSOR = 3;
 }
