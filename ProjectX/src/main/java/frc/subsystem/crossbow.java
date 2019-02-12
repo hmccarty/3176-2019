cda06e0d3781699961755a21601ef36df0b8d4f6
Jonathan Heidegger <jth3141@gmail.com>
2019-02-11 20:38:44 -0500

    2/11 Update

--- ProjectX/src/main/java/frc/subsystem/crossbow.java
+++ ProjectX/src/main/java/frc/subsystem/crossbow.java
@@ -1,13 +1,35 @@
-/*package frc.subsystem; 
+package frc.subsystem; 
 
-import frc.util.subsystem; 
+import frc.util.subsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
-public class crossbow extends subsystem {
+public class crossbow {
     private static crossbow instance = new crossbow();
 
-    public crossbow(){}
+    private DoubleSolenoid dSol;
+    private DoubleSolenoid dSol2;
+
+    public crossbow() {
+        dSol = new DoubleSolenoid(0, 1);
+        dSol2 = new DoubleSolenoid(2, 3);
+    }
+
+    public void in() {
+        dSol.set(DoubleSolenoid.Value.kForward);
+        dSol2.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public void out() {
+        dSol.set(DoubleSolenoid.Value.kReverse);
+        dSol2.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void off() {
+        dSol.set(DoubleSolenoid.Value.kOff);
+        dSol2.set(DoubleSolenoid.Value.kOff);
+    }
     
     public static crossbow getInstance(){
         return instance; 
     }
-}*/
+}
