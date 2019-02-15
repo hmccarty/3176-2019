d497a27c54c1097cd96e72aaf9c4c0df42c909a0
Voesik <martin.l.wilson789@gmail.com>
2019-02-14 21:58:25 -0500

    2/14 update

--- BenDev/src/main/java/frc/robot/CargoClaw.java
+++ BenDev/src/main/java/frc/robot/CargoClaw.java
@@ -7,6 +7,7 @@ public class CargoClaw {
     private static CargoClaw instance = new CargoClaw(2,3);
     Value dsolval;
     static DoubleSolenoid dsol;
+    Sensor clawSensor = new Sensor();
     
     public CargoClaw(int sol1, int sol2){
         dsol = new DoubleSolenoid(sol1,sol2);
@@ -15,14 +16,23 @@ public class CargoClaw {
     public static CargoClaw getInstance(){
         return instance; 
     }
-    public void forward() {
+    public void close() {
         dsol.set(DoubleSolenoid.Value.kForward);
     }
-    public void reverse() {
+    public void open() {
         dsol.set(DoubleSolenoid.Value.kReverse);
     }
     public void off() {
         dsol.set(DoubleSolenoid.Value.kOff);
     }
 
+    public void testClawSideTransfer(){
+        if(dsol.get() != DoubleSolenoid.Value.kOff) {
+            close();
+        }
+        off();
+        if(clawSensor.getValue() == 1){
+            close();
+        }
+    }
 }
