69a172e663a0a3ffa2f18d8e8a4c1426ff581358
elijah-furuness <46844817+elijah-furuness@users.noreply.github.com>
2019-03-18 20:25:01 -0400

    Testing elevator

---
+++ NeoTest/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Robot extends IterativeRobot {
+  CANSparkMax motor;
+  CANSparkMax motor1;
+  Joystick stick;
+
+  @Override
+  public void robotInit() {
+    motor = new CANSparkMax(5, MotorType.kBrushless);
+    motor1 = new CANSparkMax(4, MotorType.kBrushless);
+    stick = new Joystick(0);
+    motor.setSmartCurrentLimit(40);
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    motor.set(stick.getY());
+    motor1.follow(motor, true);
+    System.out.println(motor.getOutputCurrent());
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+}
